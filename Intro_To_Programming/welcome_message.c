cd350a5317ca77bc16f6fb7cc3cb6c83df01ad54
Tommy Ford <93237771+ceoTommy@users.noreply.github.com>
2022-03-04 12:40:33 +1030

    remade welcome message

--- Intro_To_Programming/welcome_message.c
+++ Intro_To_Programming/welcome_message.c
@@ -6,7 +6,7 @@
 /*   By: tford <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 12:52:09 by tford             #+#    #+#             */
-/*   Updated: 2022/02/28 13:27:28 by tford            ###   ########.fr       */
+/*   Updated: 2022/03/04 12:33:50 by tford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ typedef struct s_welcome_txt{
 	const char	*customer_name;
 }	t_welcome_txt;
 
-static char	*welcome_str(t_welcome_txt *wel_txt)
+//joins all contents of the welcome struct
+//into one string
+static char	*join_welcome_str(t_welcome_txt *wel_txt)
 {
 	char	*str;
 
@@ -31,23 +33,43 @@ static char	*welcome_str(t_welcome_txt *wel_txt)
 	return (str);
 }
 
-//write error message
+//name is a not NULL and all alphabetical
+int	is_valid_name(const char *str)
+{
+	if (str == NULL)
+	{
+		printf("Error: no name entered");
+		return (0);
+	}
+
+	while (*str)
+	{
+		if (!ft_isalpha(*str))
+		{
+			printf("Must be only alphabetical characters\n");
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
 
 int main()
 {
 	t_welcome_txt 	wel_txt;
-	char[512] 		scan_buf;
+	char 			scan_buf[512];
 
 	wel_txt.c_prefix = "Welcome to ";
 	wel_txt.c_name = "FDS";
 	wel_txt.c_suffix = " - ";
+	wel_txt.customer_name = NULL;
 
-	while (wel_txt.name == NULL)
+	while (wel_txt.customer_name == NULL)
 	{
 		printf("Enter your name: ");
 		scanf("%s", scan_buf);
 		if (is_valid_name(scan_buf))
 			wel_txt.customer_name = ft_strtrim(scan_buf, " ");
 	}
-	printf("%s", welcome_str(&wel_txt));
+	printf("%s", join_welcome_str(&wel_txt));
 }
